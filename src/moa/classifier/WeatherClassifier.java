eacb78799b5cb3f7e11188f496c092b1b5f809fd
Arinto Murdopo <arinto@gmail.com>
2013-01-28 18:58:04 +0100

    Add container for WeatherClassifier

---
+++ src/moa/classifier/WeatherClassifier.java
@@ -0,0 +1,45 @@
+package moa.classifier;
+
+import weka.core.Instance;
+import moa.classifiers.AbstractClassifier;
+import moa.core.Measurement;
+
+public class WeatherClassifier extends AbstractClassifier {
+
+	@Override
+	public double[] getVotesForInstance(Instance arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isRandomizable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void getModelDescription(StringBuilder arg0, int arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected Measurement[] getModelMeasurementsImpl() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void resetLearningImpl() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void trainOnInstanceImpl(Instance arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
